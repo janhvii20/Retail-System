49f937033fa8722defbe194bb6f9af9f59f56954
Janhavi <janhaviy687@gmail.com>
2025-12-09 11:00:52 +0530

    Removed node_modules & dataset

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,23 +1,6 @@
-/*import axios from 'axios';
-
-const API_BASE = 'https://retail-system-ea4f.onrender.com/api';
-
-const api = axios.create({
-  baseURL: API_BASE,
-  timeout: 10000
-});
-
-export const transactionAPI = {
-  getTransactions: (params) => api.get('/transactions', { params }),
-  getFilterOptions: () => api.get('/transactions/filter-options')
-};
-
-export default api;*/
-
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL;
-
+const API_BASE = 'https://retail-system-ea4f.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
